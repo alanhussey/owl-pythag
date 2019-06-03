95092448ead7251878f694b3df85ec13716f6f9b
Alan Hussey <alan.hussey@gmail.com>
2019-06-02 21:34:29 -0700

    update for 2019

--- bin/transform-data.js
+++ bin/transform-data.js
@@ -1,17 +1,13 @@
-const fs = require('fs');
-
+const fs = require("fs");
 
 function main() {
-    const data = JSON.parse(
-        fs.readFileSync('./data.json', {encoding: 'utf8'})
-    );
-    
-    const stages = data.data.stages;
+  const data = JSON.parse(fs.readFileSync("./2019.json", { encoding: "utf8" }));
 
-    fs.writeFileSync('./src/stages.json', JSON.stringify(stages));
+  const stages = data.data.stages;
+
+  fs.writeFileSync("./src/stages-2019.json", JSON.stringify(stages));
 }
 
-
 if (require.main === module) {
-    main();
+  main();
 }
--- src/Table.js
+++ src/Table.js
@@ -4,6 +4,7 @@ import { getTeams } from "./calculate";
 export default class Table extends Component {
   state = {
     stages: null,
+    year: new Date().getFullYear().toString(),
     includePreseason: false,
     includePlayoffs: false,
     usePoints: false,
@@ -11,8 +12,11 @@ export default class Table extends Component {
     exponent: 2
   };
   async componentDidMount() {
-    const stages = await import("./stages.json");
-    this.setState({ stages });
+    const [stages18, stages19] = await Promise.all([
+      import("./stages-2018.json"),
+      import("./stages-2019.json")
+    ]);
+    this.setState({ stages: { 2018: stages18, 2019: stages19 } });
   }
 
   toggle(option) {
@@ -24,146 +28,177 @@ export default class Table extends Component {
   }
 
   render() {
-    const { stages, exponent, ...options } = this.state;
+    const { stages, exponent, year, ...options } = this.state;
 
     const teams = stages
-      ? getTeams(stages, { ...options, exponent: Number(exponent) })
+      ? getTeams(stages[year], { ...options, exponent: Number(exponent) })
       : [];
 
     return (
-      <div className="Table">
-        <form onSubmit={event => event.preventDefault()}>
-          <label>
-            <strong>Include</strong>
-            <input
-              type="checkbox"
-              onChange={this.toggle("includePreseason")}
-              checked={options.includePreseason}
-            />
-            preseason matches
-          </label>
-          <br />
-          <label>
-            <input
-              type="checkbox"
-              onChange={this.toggle("includePlayoffs")}
-              checked={options.includePlayoffs}
-            />
-            playoff matches
-          </label>
+      <React.Fragment>
+        <div className="Table">
+          <form onSubmit={event => event.preventDefault()}>
+            <label>
+              <strong>Year</strong>
+              <select
+                onChange={event => this.setState({ year: event.target.value })}
+              >
+                <option selected={year === "2019"} value="2019">
+                  2019 (as of 2019-06-02)
+                </option>
+                <option selected={year === "2018"} value="2018">
+                  2018
+                </option>
+              </select>
+            </label>
 
-          <br />
+            <br />
 
-          <label>
-            <strong>Calculate expected wins using</strong>
-            <input
-              type="radio"
-              onChange={event => this.setState({ usePoints: false })}
-              checked={!options.usePoints}
-            />
-            game (maps) won/lost
-          </label>
-          <br />
-          <label>
-            <input
-              type="radio"
-              onChange={event => this.setState({ usePoints: true })}
-              checked={options.usePoints}
-            />
-            points for/against
-          </label>
-          <br />
+            <label>
+              <strong>Include</strong>
+              <input
+                type="checkbox"
+                onChange={this.toggle("includePreseason")}
+                checked={options.includePreseason}
+              />
+              preseason matches
+            </label>
+            <br />
+            <label>
+              <input
+                type="checkbox"
+                onChange={this.toggle("includePlayoffs")}
+                checked={options.includePlayoffs}
+              />
+              playoff matches
+            </label>
 
-          <label>
-            <strong>Exponent term (advanced)</strong>
-            <input
-              type="radio"
-              onChange={event => this.setState({ recalculateExponent: true })}
-              checked={options.recalculateExponent}
-            />
-            Recalculate per-team (<a href="https://en.wikipedia.org/wiki/Pythagenpat">Pythagenpat formula</a>)
-          </label>
-          <br />
-          <label>
-            <input
-              type="radio"
-              onChange={event => this.setState({ recalculateExponent: false })}
-              checked={!options.recalculateExponent}
-            />
-            Use a constant value (
-            <input
-              type="text"
-              style={{width: '2.5em'}}
-              value={exponent}
-              onChange={event =>
-                this.setState({ exponent: event.target.value })
-              }
-              disabled={options.recalculateExponent}
-            />
-            )
-          </label>
-          <br />
-        </form>
+            <br />
 
-        <table>
-          <thead>
-            <tr>
-              <th>Team</th>
-              <th>Matches</th>
-              <th>Record</th>
+            <label>
+              <strong>Calculate expected wins using</strong>
+              <input
+                type="radio"
+                onChange={event => this.setState({ usePoints: false })}
+                checked={!options.usePoints}
+              />
+              game (maps) won/lost
+            </label>
+            <br />
+            <label>
+              <input
+                type="radio"
+                onChange={event => this.setState({ usePoints: true })}
+                checked={options.usePoints}
+              />
+              points for/against
+            </label>
+            <br />
 
-              <th>Delta</th>
-              <th>Expected</th>
+            <label>
+              <strong>Exponent term (advanced)</strong>
+              <input
+                type="radio"
+                onChange={event => this.setState({ recalculateExponent: true })}
+                checked={options.recalculateExponent}
+              />
+              Recalculate per-team (
+              <a href="https://en.wikipedia.org/wiki/Pythagenpat">
+                Pythagenpat formula
+              </a>
+              )
+            </label>
+            <br />
+            <label>
+              <input
+                type="radio"
+                onChange={event =>
+                  this.setState({ recalculateExponent: false })
+                }
+                checked={!options.recalculateExponent}
+              />
+              Use a constant value (
+              <input
+                type="text"
+                style={{ width: "2.5em" }}
+                value={exponent}
+                onChange={event =>
+                  this.setState({ exponent: event.target.value })
+                }
+                disabled={options.recalculateExponent}
+              />
+              )
+            </label>
+            <br />
+          </form>
 
-              <th>
-                <abbr title="Games (maps) won">GW</abbr>
-              </th>
-              <th>
-                <abbr title="Games (maps) lost">GL</abbr>
-              </th>
+          <table>
+            <thead>
+              <tr>
+                <th>Team</th>
+                <th>Matches</th>
+                <th>Record</th>
 
-              <th>
-                <abbr title="Points for">PF</abbr>
-              </th>
-              <th>
-                <abbr title="Points against">PA</abbr>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {teams.map(team => (
-              <tr key={team.name}>
-                <td style={{ textAlign: "left" }}>
-                    <img width="16" height="16" src={team.team.info.icon} alt="" />
-                    {team.name}
-                </td>
-                <td style={{ textAlign: "right" }}>{team.totalMatches}</td>
-                <td style={{ textAlign: "right" }}>
-                  {team.matchesWon} - {team.matchesLost}
-                </td>
-                <td
-                  style={{
-                    color:
-                      team.expectedWins - team.matchesWon > 0 ? "green" : "red",
-                    textAlign: "right"
-                  }}
-                >
-                  {team.expectedWins - team.matchesWon > 0 ? "+" : ""}
-                  {(team.expectedWins - team.matchesWon).toFixed(1)}
-                </td>
-                <td style={{ textAlign: "right" }}>
-                  {team.expectedWins.toFixed(1)} -{" "}
-                  {(team.totalMatches - team.expectedWins).toFixed(1)}
-                </td>
-                <td style={{ textAlign: "right" }}>{team.gamesWon}</td>
-                <td style={{ textAlign: "right" }}>{team.gamesLost}</td>
-                <td style={{ textAlign: "right" }}>{team.pointsEarned}</td>
-                <td style={{ textAlign: "right" }}>{team.pointsAllowed}</td>
+                <th>Delta</th>
+                <th>Expected</th>
+
+                <th>
+                  <abbr title="Games (maps) won">GW</abbr>
+                </th>
+                <th>
+                  <abbr title="Games (maps) lost">GL</abbr>
+                </th>
+
+                <th>
+                  <abbr title="Points for">PF</abbr>
+                </th>
+                <th>
+                  <abbr title="Points against">PA</abbr>
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {teams.map(team => (
+                <tr key={team.name}>
+                  <td style={{ textAlign: "left" }}>
+                    <img
+                      width="16"
+                      height="16"
+                      src={team.team.info.icon}
+                      alt=""
+                    />
+                    {team.name}
+                  </td>
+                  <td style={{ textAlign: "right" }}>{team.totalMatches}</td>
+                  <td style={{ textAlign: "right" }}>
+                    {team.matchesWon} - {team.matchesLost}
+                  </td>
+                  <td
+                    style={{
+                      color:
+                        team.expectedWins - team.matchesWon > 0
+                          ? "green"
+                          : "red",
+                      textAlign: "right"
+                    }}
+                  >
+                    {team.expectedWins - team.matchesWon > 0 ? "+" : ""}
+                    {(team.expectedWins - team.matchesWon).toFixed(1)}
+                  </td>
+                  <td style={{ textAlign: "right" }}>
+                    {team.expectedWins.toFixed(1)} -{" "}
+                    {(team.totalMatches - team.expectedWins).toFixed(1)}
+                  </td>
+                  <td style={{ textAlign: "right" }}>{team.gamesWon}</td>
+                  <td style={{ textAlign: "right" }}>{team.gamesLost}</td>
+                  <td style={{ textAlign: "right" }}>{team.pointsEarned}</td>
+                  <td style={{ textAlign: "right" }}>{team.pointsAllowed}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </React.Fragment>
     );
   }
 }
